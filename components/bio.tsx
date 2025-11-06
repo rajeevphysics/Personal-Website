b5f26a6698e96313e283c9ffade548137c1c25d7
Rajeev <ra2066shopping@gmail.com>
2025-11-05 19:24:11 -0500

    fixed animation on bio

--- components/bio.tsx
+++ components/bio.tsx
@@ -1,41 +1,64 @@
+
 "use client"
+import { motion, useInView } from "framer-motion"
+import { useRef } from "react"
 import RoundedButton from "./anim/rounded-button"
+import { slideUp, opacity } from "./anim/anim"
 
 export default function Bio() {
-  const phrase1 = "An undergraduate experimental physicist bridging " + "theory and experiment."
+  const phrase1 = "An undergraduate experimental physicist bridging theory and experiment."
 
   const phrase2 =
-    "Particularly interested the fields of optical and quantum systems, as well as condensed matter." +
-    "\n I believe that these disciplines will lead to new " +
-    "discoveries about the world."
+    "Particularly interested the fields of optical and quantum systems, as well as condensed matter. I believe that these disciplines will lead to new discoveries about the world."
 
-
+  const description = useRef(null)
+  const isInView = useInView(description)
 
   return (
-    <div className="relative flex flex-col justify-center gap-8 p-8 pb-24 sm:mt-[50px] sm:flex-row sm:p-20 sm:pb-32">
-      <div className="space-y-1 z-50">
-        <p className="m-0 gap-2 leading-snug text-2xl sm:text-4xl  mb-4 ">
+    <div
+      ref={description}
+      className="relative flex flex-col justify-center gap-8 p-8 pb-24 sm:mt-[50px] sm:flex-row sm:p-20 sm:pb-32"
+    >
+      <div className="space-y-1 font-sans">
+        <p className="m-0 gap-2 text-2xl leading-snug sm:text-4xl">
           {phrase1.split(" ").map((word, index) => (
-            <span key={index} className="relative mr-1.5 inline-flex">
-              {word}
+            <span key={index} className="relative mr-1.5 inline-flex overflow-hidden">
+              <motion.span custom={index} variants={slideUp} animate={isInView ? "open" : "closed"}>
+                {word}
+              </motion.span>
             </span>
           ))}
         </p>
-        <p className="m-0 gap-2 leading-snug text-2xl sm:text-4xl">
+        <p className="m-0 gap-2 text-2xl leading-snug sm:text-4xl">
           {phrase2.split(" ").map((word, index) => (
-            <span key={index} className="relative mr-1.5 inline-flex">
-              {word}
+            <span key={index} className="relative mr-1.5 inline-flex overflow-hidden">
+              <motion.span
+                custom={index + phrase1.split(" ").length}
+                variants={slideUp}
+                animate={isInView ? "open" : "closed"}
+              >
+                {word}
+              </motion.span>
             </span>
           ))}
         </p>
       </div>
-      <div>
-        <p className="m-0 pb-3 font-light text-base sm:text-large z-50">
-          A 2nd Year student @ The University of Waterloo .
-        </p>
-        <p className="m-0 text-base font-light sm:text-large z-50">
-          I’ve led educational and simulation-based projects focused on topics like gravitational modeling and precision sensing, while also contributing to rocketry payload resarch and design. 
-        </p>
+      <div className="font-sans">
+        <motion.p
+          variants={opacity}
+          animate={isInView ? "open" : "closed"}
+          className="m-0 pb-3 text-base font-light sm:text-lg"
+        >
+          Currently a 2nd Year student @ The University of Waterloo .
+        </motion.p>
+        <motion.p
+          variants={opacity}
+          animate={isInView ? "open" : "closed"}
+          className="m-0 text-base font-light sm:text-lg"
+        >
+          I've led educational and simulation-based projects focused on topics like classical physics and gravitational
+          waves, while also contributing to rocketry payload research and design.
+        </motion.p>
         <div data-scroll-speed={0.1}>
           <RoundedButton
             href="/about"
